7f2da6c6f65fd869214d3489941b551f5afdfaa5
open.g <870519245@qq.com>
2024-09-11 18:35:13 +0800

    ref: 更改代码

--- Program.cs
+++ Program.cs
@@ -3,37 +3,34 @@ using MyNamespace;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using System.IO;
-namespace MyNamespace
+
+class Program
 {
+    static IConfigurationRoot configuration;
 
-    class Program
+    static async Task Main(string[] args)
     {
-        static IConfigurationRoot configuration;
+        // 设置配置文件路径
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
-        static async Task Main(string[] args)
-        {
-            // 设置配置文件路径
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        configuration = builder.Build();
 
-            configuration = builder.Build();
+        // 读取配置值
+        string appName = configuration["MyAppSettings:ApplicationName"];
+        string version = configuration["MyAppSettings:Version"];
 
-            // 读取配置值
-            string appName = configuration["MyAppSettings:ApplicationName"];
-            string version = configuration["MyAppSettings:Version"];
+        Console.WriteLine($"App Name: {appName}, Version: {version}");
 
-            Console.WriteLine($"App Name: {appName}, Version: {version}");
+        MyClass myClassInstance = new MyClass();
 
-            MyClass myClassInstance = new MyClass();
+        List<int> result = await myClassInstance.GetDataAsync();
 
-            List<int> result = await myClassInstance.GetDataAsync();
+        // myClassInstance.ArrayMethod();
+        // myClassInstance.ListMethod();
+        // myClassInstance.DictionaryMethod();
+        // myClassInstance.LinqMethod();
 
-            // myClassInstance.ArrayMethod();
-            // myClassInstance.ListMethod();
-            // myClassInstance.DictionaryMethod();
-            // myClassInstance.LinqMethod();
-
-        }
     }
 }
